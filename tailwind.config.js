f1f913d3c5fc6facb8182960ea48801647eadbd7
John Abrahams <jabrah23@gmail.com>
2022-05-21 13:31:22 -0400

    Add Tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  content: [
+    './pages/**/*.{js,jsx,ts,tsx}',
+    './components/**/*.{js,jsx,ts,tsx}',
+    // For the best performance and to avoid false positives,
+    // be as specific as possible with your content configuration.
+  ],
+};

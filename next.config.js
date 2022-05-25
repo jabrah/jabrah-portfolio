12f83ad88a51ce06be8721675a4bcf47dd03df2d
John Abrahams <jabrah23@gmail.com>
2022-05-25 17:30:02 -0400

    Change base path, update GH action

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
+const isProd = process.env.NODE_ENV === 'production';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  basePath: isProd ? '/jabrah-portfolio' : '',
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;

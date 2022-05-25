8af77b97b7929d3ae22d5057198a8aae3adb575c
John Abrahams <jabrah23@gmail.com>
2022-05-25 19:07:07 -0400

    Pin Node version. Add a NextJS comment

--- next.config.js
+++ next.config.js
@@ -2,6 +2,14 @@ const isProd = process.env.NODE_ENV === 'production';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  /**
+   * Had a problem in GH Pages with the static export links pointing to the wrong place.
+   * Adding the basePath fixed this
+   * 
+   * The path should simply be the repo name
+   * 
+   * https://nextjs.org/docs/api-reference/next.config.js/cdn-support-with-asset-prefix
+   */
   basePath: isProd ? '/jabrah-portfolio' : '',
   reactStrictMode: true,
 };

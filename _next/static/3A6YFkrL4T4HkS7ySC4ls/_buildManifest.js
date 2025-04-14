1705b07103d33ea3dcd99bdd0b9788161a1a2258
jabrah <jabrah@users.noreply.github.com>
2025-04-14 14:20:02 +0000

    deploy: 823cd9af52ebd696a1d7a5130f606543b2e5c022

--- _next/static/3A6YFkrL4T4HkS7ySC4ls/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/1bfc9850-a84f7a2283d4cf05.js","static/css/40ef4ef489e6c342.css","static/chunks/pages/index-fe733120842a531d.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();

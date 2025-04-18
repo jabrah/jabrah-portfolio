84a1581091c3c8daf12a08a5a817e5e1dc092a7e
John Abrahams <jabrah20@jhu.edu>
2025-04-18 15:39:13 -0400

    Skills logos styles and animation

--- components/skills/Skill.js
+++ components/skills/Skill.js
@@ -1,18 +1,23 @@
 import IconFinder from "../svg/IconFinder";
+import * as motion from "motion/react-client"
 
 export default function Skill({ children, link, icon, iWidth, iHeight, title }) {
   iWidth = iWidth || 30;
   iHeight = iHeight || 30;
 
   return (
-    <div className="rounded-md hover:shadow-lg" title={title}>
+    <motion.div
+        className="rounded-md m-4 bg-gray-800 content-center hover:shadow-lg hover:shadow-myblue-300/10"
+        title={title}
+        whileHover={{scale: 1.1 }}
+      >
       <LinkWrapper link={link} className="flex space-y-2 p-6">
         <div className="">
           <IconFinder icon={icon} width={iWidth} height={iHeight} />
         </div>
         <div className="italic">{children}</div>
       </LinkWrapper>
-    </div>
+    </motion.div>
   );
 }
 
@@ -26,7 +31,14 @@ function LinkWrapper({ children, className, link }) {
     <>
       {
         !!link ? 
-          <a className={className} href={link}>{children}</a>
+          <a
+              className={className}
+              href={link}
+              target="_blank"
+              rel="noreferrer"
+            >
+            {children}
+          </a>
           : <div className={className}>{children}</div>
       }
     </>
